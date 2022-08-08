9ecb0d9ae512a566b26dfa844da53941896235f5
javid <norouzi.javid@protonmail.com>
2022-08-09 03:46:16 +0430

    mask error fixed

--- nets/sampler.py
+++ nets/sampler.py
@@ -471,9 +471,8 @@ class DoubleDescNghSampler2(nn.Module):
         b2 = b1
         xy2 = (aflow[b1, :, y1, x1] + 0.5).long().t()
         mask = (0 <= xy2[0]) * (0 <= xy2[1]) * (xy2[0] < W) * (xy2[1] < H)
-        mask = mask
         mask = mask.view(shape)
-
+        mask = mask.repeat(2, 1)
         def clamp(xy):
             torch.clamp(xy[0], 0, W - 1, out=xy[0])
             torch.clamp(xy[1], 0, H - 1, out=xy[1])
